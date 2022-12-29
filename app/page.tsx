a76ba2c8be6810d979e4b433bdbedd6f0404a3c8
Sami Amsaf <samiamsaf1902@gmail.com>
2022-12-29 17:53:50 -0500

    Inital commit

---
+++ app/page.tsx
@@ -0,0 +1,18 @@
+export default function RegistrationPage() {
+  return (
+    <h1 className="h-screen w-screen">
+      <nav className="h-[4rem] w-full flex items-center mt-4">
+        <img
+          src="/msu-logo.svg"
+          alt="Picture of the author"
+          className="h-[4rem] ml-5 mr-2"
+        />
+        <img
+          src="/msu-marketplace-logo.svg"
+          alt="Picture of the author"
+          className="h-[3rem]"
+        />
+      </nav>
+    </h1>
+  );
+}
